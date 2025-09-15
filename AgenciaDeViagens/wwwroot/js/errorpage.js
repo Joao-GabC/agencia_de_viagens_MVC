d2d9021479da3a2dc8391cc6ac9b8d425736df1e
jotage-c <maninhodoisdois25@gmail.com>
2025-09-15 13:26:36 -0300

    .

--- AgenciaDeViagens/wwwroot/js/errorpage.js
+++ AgenciaDeViagens/wwwroot/js/errorpage.js
@@ -1,5 +1,13 @@
-﻿document.addEventListener("DOMContentLoaded", () => {
+﻿const countdown = document.getElementById("countdown");
+var countdownNum = 10;
+
+document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         window.location.replace("/Home/Index");
-    }, 5000);
+    }, 10000);
+    for (i = 10; i < 10; i++) {
+        setTimeout(() => {
+            countdown.innerHTML = countdownNum;
+        }, 1000);
+    }
 })
